38d3a78adecf07a926d17af94995a08c87e6bd10
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2020-11-29 14:09:32 -0800

    there are two servers. One for routes and other for auth

--- src/servers/authServer.ts
+++ src/servers/authServer.ts
@@ -9,7 +9,7 @@ import signIn from '../routes/auth/signin'
 import signUp from '../routes/auth/signup'
 import signOut from '../routes/auth/signout'
 
-const port = process.env.MONGO_URI || 4001
+const port = process.env.PORT || process.env.MONGO_AUTH
 
 mongoose.set('useCreateIndex', true);
 
@@ -23,7 +23,7 @@ server.use('/auth', signIn)
 server.use('/auth', signUp)
 server.use('/auth', signOut)
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to db'))
+mongoose.connect(process.env.MONGO_AUTH, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to db'))
 mongoose.connection
   .once('open', () => console.log('connection to mongoDb successful'))
   .on('error', (err) => {
--- src/servers/mainServer.ts
+++ src/servers/mainServer.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-const port = 8000
 import 'dotenv/config'
 import getList from '../routes/pao/getList'
 import putList from '../routes/pao/putList'
@@ -10,6 +9,7 @@ import deleteUserAccount from '../routes/pao/deleteUserAccount'
 import putItem from '../routes/pao/putItem'
 import updateItem from '../routes/pao/updateItem'
 
+const port = process.env.PORT || process.env.MONGO_MAIN
 
 mongoose.set('useCreateIndex', true);
 
@@ -21,13 +21,13 @@ server.use(express.json())
 server.use('/lists', updateItem, getList, putList, putItem, deleteItem)
 server.use('/', deleteUserAccount)
 
-mongoose.connect(process.env.DENNIS, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to db'))
+mongoose.connect(process.env.MONGO_MAIN, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to db'))
 mongoose.connection
   .once('open', () => console.log('connection to mongoDb successful'))
   .on('error', (err) => {
     console.log(err, 'err in connecting to mongoDb')
   })
 
-server.listen(port, () => {
+server.listen(process.env.PORT, () => {
   console.log(`server started at http://localhost:${port}`)
 })
